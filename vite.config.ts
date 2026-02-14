00dc73371b95a3d80b24ae23bf6be07da67e3214
Borngreat Ikwutah <iagocech2056@gmail.com>
2026-02-14 23:19:47 +0100

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 import viteReact from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
@@ -17,6 +18,7 @@ export default defineConfig({
         routesDirectory: "routes",
       },
     }),
+    nitro(),
     viteReact(),
   ],
 });
